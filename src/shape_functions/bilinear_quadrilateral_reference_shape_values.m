9cd41db3f0beb0a99c7716d43e4a4f89aef375d5
LAPTOP-AN7IO5HB\ROG <zjb2024ok@yeah.net>
2025-11-05 16:29:33 +0100

    test

--- src/shape_functions/bilinear_quadrilateral_reference_shape_values.m
+++ src/shape_functions/bilinear_quadrilateral_reference_shape_values.m
@@ -3,7 +3,7 @@ function N = bilinear_quadrilateral_reference_shape_values(xi)
 % Calculate the shape function values for a bilinear quadrilateral element. 
 % Input: xi [2x1] - local coordinates
 % Output: N [1x4] - shape function values at xi
-
+%try git
     if size(xi,1) ~= 2 || size(xi,2) ~= 1
         error("xi must be 2x1")
     end
